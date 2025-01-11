ebd2f2ce9a8f6e4393e7c0d69592874ddf0a347c
Fima41 <98807101+Fima41@users.noreply.github.com>
2025-01-11 14:35:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,18 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    target: 'esnext',
+    cssCodeSplit: true,
     rollupOptions: {
+      input: {
+        main: './index.html'
+      },
       output: {
         assetFileNames: 'assets/[name][extname]',
         chunkFileNames: 'assets/[name].js',
-        entryFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js'
       }
     }
   }
