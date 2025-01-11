3297554478490fe44c4bd3ecf7924b426b5aa84f
Fima41 <fimahsichone41@gmail.com>
2025-01-11 09:06:28 +0200

    Fix build configuration and add type module to script tag

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        chunkFileNames: 'assets/[name].js',
+        entryFileNames: 'assets/[name].js',
+      }
+    }
+  }
+})
